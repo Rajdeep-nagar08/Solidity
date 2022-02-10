8f7c8f9effaaa26afc3dbbef8d3b05c1c6b3863b
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-02-10 18:29:17 +0530

    Create Constants.sol

---
+++ Solidity/Constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract constants{
+
+    address public constant owneradd1=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;  //low gas
+    address public owneradd2=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // high gas 
+    
+}
