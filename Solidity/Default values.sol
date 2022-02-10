6545649b4797969ac50a7ce530d07fe348cb4dfb
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-02-10 18:28:03 +0530

    Create Default values.sol

---
+++ Solidity/Default values.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract defaultValue{
+    uint public x;
+    bool public y;
+    address public z;
+    bytes32 public a;
+    string public str;
+
+}
