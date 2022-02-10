e83c78edc2eef8f7f40b111aa6afce0d3ce752f2
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-02-10 21:24:33 +0530

    Create struct 3.sol

---
+++ Solidity/struct 3.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+    struct s{
+
+        string name;
+        uint age;
+        address add;
+
+    }
+
+    contract Structs1{
+       s public p1=s("Nidhi",25,0x5B38Da6a701c568545dCfcB03FcB875f56beddC4) ;
+
+    }
+
+    contract struct2{
+       s public p1=s("Gagan",20,0x5B38Da6a701c568545dCfcB03FcB875f56beddC4) ;
+
+    }
+
+
